195b3d5d2dbab571a8003ba44038c9db2449e24b
Pedro Lucas <pedronun@protonmail.com>
2023-02-14 23:21:23 -0300

    feat: feito banner e titulo do filme na pagina do filme

--- src/pages/Movie/Movie.styles.ts
+++ src/pages/Movie/Movie.styles.ts
@@ -1,7 +1,44 @@
 import styled from "styled-components/native";
 import { theme } from "../../global/global";
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.ScrollView`
   flex: 1;
   background-color: ${theme.colors.primary};
+`;
+
+export const Content = styled.View`
+  margin: 10px 24px 0;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 80px;
+  left: 30px;
+  z-index: 1;
+  background-color: ${theme.colors.highlight};
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${theme.fonts.title700};
+  font-size: 32px;
+  line-height: 53px;
+  color: ${theme.colors.highlight};
+`;
+
+export const MovieContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const MovieTitle = styled.Text`
+  font-family: ${theme.fonts.title700};
+  font-size: 32px;
+  line-height: 53px;
+  color: ${theme.colors.highlight};
 `;
--- src/pages/Movie/Movie.tsx
+++ src/pages/Movie/Movie.tsx
@@ -1,21 +1,77 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Image, View, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Container } from './Movie.styles';
-import { useQuery } from 'react-query';
-import api from '../../services/api';
-import { API_KEY } from '@env';
-import { IMovie } from '../../@types/movie';
+import { BackButton, Container, Content, MovieContent, Title } from "./Movie.styles";
+import api from "../../services/api";
+import { API_KEY } from "@env";
+import { IMovie } from "../../@types/movie";
+import { Ionicons } from "@expo/vector-icons";
+import { theme } from "../../global/global";
+import { LinearGradient } from "expo-linear-gradient";
+import { MovieCompany, MovieText } from "../../components/NewRelease/NewRelease.styles";
+import { useQuery } from "react-query";
 
 export function Movie({ route }: any) {
   const movieId = route.params.movieId;
   const navigation = useNavigation();
 
-  return (
+  const {
+    data: movie,
+    isError,
+    isLoading,
+  } = useQuery<IMovie>("movie", async () => {
+    const response = await api.get(`/Title/${API_KEY}/${movieId}/Posters,Trailer,`);
+    return response.data;
+  });
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  return isError && movie ? (
     <Container>
-      {/* <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Go back</Text>
-      </TouchableOpacity> */}
-      <Text>Movie</Text>
+      <BackButton onPress={() => navigation.goBack()}>
+        <Ionicons name="arrow-back" size={32} color={theme.colors.primary} />
+      </BackButton>
+      <Image
+        source={{ uri: movie.image }}
+        style={{ width: "100%", height: 400 }}
+      />
+      <LinearGradient
+        colors={["transparent", theme.colors.primary]}
+        style={{
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          zIndex: 1,
+          height: 100,
+          position: "absolute",
+          top: 300,
+        }}
+      />
+      <Content>
+        <MovieContent>
+          <View>
+            <Title>{movie?.title?.split(":")[0]}</Title>
+            <MovieCompany>{movie.companyList[0].name}</MovieCompany>
+          </View>
+          <View style={{ alignItems: "flex-end" }}>
+            <Image
+              source={require("../../assets/stars.png")}
+              style={{ width: 80, height: 16 }}
+              resizeMode={"contain"}
+            />
+            <MovieText>
+              From{" "}
+              {parseInt(
+                movie.imDbRatingVotes ? movie.imDbRatingVotes : "0"
+              ) >= 500
+                ? "+500"
+                : movie.imDbRatingVotes}{" "}
+              users
+            </MovieText>
+          </View>
+        </MovieContent>
+      </Content>
     </Container>
-  )
+  ) : null;
 }
